1a9c8ab20f5212a0658573a43a194a800c11ec12
Minju Jeon <odeto1310@gmail.com>
2025-01-03 23:10:44 +0900

    Fix application.yml security code

---
+++ src/main/java/com/cos/securityPractice/config/WebMvcConfig.java
@@ -0,0 +1,24 @@
+package com.cos.securityPractice.config;
+
+import org.springframework.boot.web.servlet.view.MustacheViewResolver;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    //mustache를 사용할건데, templates에서 .html 형식을 이용해서 개발을 진행할거라,
+    // .mustache 확장자를 찾는 경로 문제를 해결하기 위한 부분
+
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        MustacheViewResolver resolver = new MustacheViewResolver();
+        resolver.setCharset("UTF-8");
+        resolver.setContentType("text/html:charset=UTF-8");
+        resolver.setPrefix("classpath:/templates/");
+        resolver.setSuffix(".html");
+
+        registry.viewResolver(resolver);
+    }
+}
--- src/main/java/com/cos/securityPractice/controller/IndexController.java
+++ src/main/java/com/cos/securityPractice/controller/IndexController.java
@@ -1,4 +1,17 @@
 package com.cos.securityPractice.controller;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller //View를 리턴하겠다.
 public class IndexController {
+
+    //localhost:8080/
+    @GetMapping({"","/"})
+    public String index(){
+        //mustache 기본폴더 src/main/resources
+        //뷰 리졸버 설정 : templates(prefix), .mustache(suffix)로 설정되게 되어있음
+        return "index"; // src/main/resources/templates/index.mustache를 찾을 것임
+    }
 }
